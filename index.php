cb7784d43433d52da0f116d70ca3f333c7129d6e
Ronald Jimenez <Ronaldpelaez201@gmail.com>
2024-11-07 16:26:27 -0500

    feat: correción de errores

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
 
 echo "hola rr";
 echo "Vamos a realizar un cambio de codigo completo 2";
-
+echo ";";
 
 ?>
--- release.config.js
+++ release.config.js
@@ -1,27 +1,18 @@
 const config = {
-  branches: ['main'],
-  plugins: [
-    [
+    branches: ['main'],
+    plugins: [
       '@semantic-release/commit-analyzer',
-      {
-        preset: 'conventionalcommits',
-        releaseRules: [
-          { type: 'feat', release: 'minor' },
-          { type: 'fix', release: 'patch' },
-          { breaking: true, release: 'major' }
-        ],
-      },
-    ],
-    '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/git',
-      {
-        assets: ['dist/**/*', 'package.json', 'CHANGELOG.md'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ],
-    '@semantic-release/github'
-  ]
-};
-
-module.exports = config;
+      '@semantic-release/release-notes-generator',
+      [
+        '@semantic-release/git',
+        {
+          assets: ['dist/**/*', 'package.json', 'CHANGELOG.md'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
+        }
+      ],
+      '@semantic-release/github'
+    ]
+  };
+  
+  module.exports = config;
+  
